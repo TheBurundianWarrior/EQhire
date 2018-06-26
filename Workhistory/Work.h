0580d75ff18bea01dbf6ae818431c59b8115cf62
Niyongere Florentin <florentinniyongere.fn@gmail.com>
2018-06-25 20:23:50 -0500

    Header

--- Workhistory/Work.h
+++ Workhistory/Work.h
@@ -7,10 +7,25 @@ class Work
     public:
         Work();
         virtual ~Work();
+        WORK_H(string,int);
+string get CompanyName()
+string get Role()
+string get Location()
+string get Description()
+int get StartDate()
+int get EndDate()
 
-    protected:
+
+
 
     private:
+        string newCompanyName;
+        string newRole;
+        string newLocation;
+        string newDescription;
+        int newStartDate;
+        int newEndDate;
+
 };
 
 #endif // WORK_H
